b28e825e06458d3876f8916cb252b24367fd89fe
Andres Gil Vicente <202304626@alu.comillas.edu>
2026-09-09 17:51:21 +0200

    Exercise 2: Model Architectures - baseline_cnn.py

--- src/models/baseline_cnn.py
+++ src/models/baseline_cnn.py
@@ -18,7 +18,20 @@ class BaseCNN(BaseImageClassifier):
 
         super().__init__(num_classes)
 
-        raise NotImplementedError  # TODO
+        # TODO
+
+        avgpool_output_size: int = 4  # Specified in this function's docstring
+        output_feature_maps: int = 128  # Showed in the README diagram
+
+        self.cnn = self._get_cnn()
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=avgpool_output_size)
+
+        self.classifier = self._get_classifier(
+            num_classes=num_classes,
+            dropout=dropout,
+            in_features=output_feature_maps*(avgpool_output_size)**2
+        )  # The classifier is used after the flatten, so the in_features dimension is the result of multiplying: o x h x w = 128 x 4 x 4
 
     @staticmethod
     def _get_cnn() -> nn.Sequential:
@@ -29,3 +42,29 @@ class BaseCNN(BaseImageClassifier):
         """
 
         # TODO
+
+        cnn = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=32,
+                kernel_size=3
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+        return cnn
